b167b4cbadb0b1d5518d36ec24b1f54c5c64832a
Jose Castellano <jcastellanosierras@gmail.com>
2023-01-30 20:56:14 +0100

    Structured and added live-server

--- src/snake.js
+++ src/snake.js
@@ -3,6 +3,7 @@ import start from './utils.js'
 document.getElementById('start-button').onclick = newGame;
 
 function newGame(){
+	console.log("Hola");
     document.getElementById('menu').style.display = 'none';
     document.getElementById('game').style.display = 'block';
     start();
